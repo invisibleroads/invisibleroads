107ee2b46a901407d1374204dcb07015b38b2810
Roy Hyunjin Han <rhh@crosscompute.com>
2015-10-23 01:42:33 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst', 'CHANGES.rst'])
 setup(
     name='invisibleroads',
-    version='0.1',
+    version='0.1.1',
     description='Simple framework for extensible command line scripts',
     long_description=DESCRIPTION,
     classifiers=[
