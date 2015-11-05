de99cb3a7aafa2d1f6b744a89263ac5cb959618b
Roy Hyunjin Han <rhh@crosscompute.com>
2015-11-05 13:41:47 -0500

    Prepare for PyPI

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst', 'CHANGES.rst'])
 setup(
     name='invisibleroads',
-    version='0.1.2',
+    version='0.1.3',
     description='Simple framework for extensible command line scripts',
     long_description=DESCRIPTION,
     classifiers=[
