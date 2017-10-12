6bc15f265418eef2857dd93aca30e34906091fd7
Roy Hyunjin Han <rhh@crosscompute.com>
2017-10-12 07:57:15 -0400

    Add comma

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     setup_requires=[
-        'pytest-runner'
+        'pytest-runner',
     ],
     install_requires=[
         'invisibleroads-macros>=0.8.3',
